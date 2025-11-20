416998348d1cd4a7d6bcdebd225b6fbff1b44a2b
Metin Türk <metinturk553@gmail.com>
2025-11-20 23:18:02 +0300

    20.11.2025 java dersinde generic mimarisini işledik.

---
+++ Hafta4/src/main/java/com/mycompany/hafta4/Box.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hafta4;
+
+/**
+ *
+ * @author Metin
+ */
+public class Box <T> {
+
+    
+    public T getAdi() {
+        return adi;
+    }
+
+
+    public void setAdi(T adi) {
+        this.adi = adi;
+    }
+    private T adi;
+}
--- Hafta4/src/main/java/com/mycompany/hafta4/Hafta4.java
+++ Hafta4/src/main/java/com/mycompany/hafta4/Hafta4.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.hafta4;
 
+import java.util.*;
+
 /**
  *
  * @author Metin
@@ -16,6 +18,28 @@ public class Hafta4 {
         o1.GezmeyeGider();
         o1.SinavlaraGir();
         o1.KonusmaYapar("deneme ogrenci", 5);
+        
+        Ogrenci ogr = new Ogrenci(5);
+        
+        
+        List<Ogrenci> ogrliste = new ArrayList<>();
+        
+        List<Integer> notlar = new ArrayList<>();
+        
+        List ogrliste1 = new ArrayList();
+        
+        
+        
+        ogrliste.add(ogr);
+        
+        ogrliste1.add(1);
+        ogrliste1.add("deneme");
+        ogrliste1.add(ogr);
+        
+        
+        Box<Integer> b = new Box();
+        Box<Ogrenci> b1 = new Box();
+        b1.setAdi(ogr);
         
         
         
